521c3c3866e5bdcdfe3a7dbd562ab23d516bbeaa
jx2222 <110435240+jx2222@users.noreply.github.com>
2023-04-13 10:46:08 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.c5kkmgndrvtk.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "leongjingxuan-employee1"
+customregion = "us-east-1"
